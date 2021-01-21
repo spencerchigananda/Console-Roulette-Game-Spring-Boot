107150f62c881e3eb45af80df39f2cd6480e85e3
Spencer Chigananda <schigananda@bancabc.co.zw>
2021-01-21 12:37:28 +0200

    Modify Spring Boot application to implement CommandLineRunner

--- src/main/java/com/spencerchigananda/consoleroulettegame/ConsoleRouletteGameApplication.java
+++ src/main/java/com/spencerchigananda/consoleroulettegame/ConsoleRouletteGameApplication.java
@@ -1,13 +1,23 @@
 package com.spencerchigananda.consoleroulettegame;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class ConsoleRouletteGameApplication {
+public class ConsoleRouletteGameApplication implements CommandLineRunner {
+
+	/*public static void main(String[] args) {
+		SpringApplication.run(ConsoleRouletteGameApplication.class, args);
+	}*/
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsoleRouletteGameApplication.class, args);
+		new SpringApplicationBuilder(ConsoleRouletteGameApplication.class).headless(false).run(args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
